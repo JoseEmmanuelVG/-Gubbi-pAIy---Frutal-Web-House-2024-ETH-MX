cffc434e021e8414fbd23fe1ace9995dbd2400ea
JoseEmmanuelVG <89156254+JoseEmmanuelVG@users.noreply.github.com>
2024-09-11 00:13:04 -0600

    Correcting active session link, showing user and wallet

--- cte-react-gubbi/src/App.js
+++ cte-react-gubbi/src/App.js
@@ -1,36 +1,19 @@
 //// app.js /////
 
 import './index.css';
-import React, { useContext } from 'react';
-import UserContext from './context/user-context';
+import React from 'react';
 import Headernavbar from './components/layout/headbar';
 import SignInPage from './components/users/signin';
 import SignOutPage from './components/users/signout';
 import SignUpPage from './components/users/signup';
 import Transferir from './components/pagos/transferencia';
-import { Route, Routes } from 'react-router-dom'; // Elimina Router si no lo usas
+import { Route, Routes } from 'react-router-dom'; // No necesitas importar Router aquí
+import { UserContextProvider } from './context/user-context'; // Asegúrate de importar correctamente
 
 function App() {
-  const user = useContext(UserContext);
-
-  user.login = (usuario) => {
-    user.username = usuario.username;
-    user.token = usuario.token;
-    user.publickey = usuario.publickey;
-    user.cellnumber = usuario.cellnumber;
-    console.log('Ahora el usuario registrado en la app tiene estos datos:', usuario);
-  };
-
-  user.logout = () => {
-    user.username = null;
-    user.token = null;
-    user.publickey = null;
-    user.cellnumber = null;
-    console.log('Ahora no hay usuario registrado en la app:', user);
-  };
-
   return (
-    <UserContext.Provider value={user}>
+    // Asegúrate de que UserContextProvider esté envolviendo la aplicación
+    <UserContextProvider>
       <div className="flex flex-col min-h-screen bg-gray-100 text-gray-900">
         <Headernavbar />
         <main className="flex-grow">
@@ -42,7 +25,7 @@ function App() {
           </Routes>
         </main>
       </div>
-    </UserContext.Provider>
+    </UserContextProvider>
   );
 }
 
